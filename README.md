a95a9500edd72381ea0cc23ab11948fd1c4cafad
DreamEcho100 <maze6572198@gmail.com>
2021-03-13 21:16:58 +0200

    Uplaod&Update...

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
+<https://sequelize.org/v4/manual/tutorial/migrations.html>
+
 # web-chat-application-server
 
 Follwing the 'Node.js and React: Build a complete web chat application' Udemy course.
+
+##
+
+### Chat model
+
+sequelize model:create --name Chat --attributes type:string
+
+### ChatUser model
+
+sequelize model:create --name ChatUser --attributes chatId:integer,userId:integer
+
+### Message model
+
+sequelize model:create --name Message --attributes type:string,message:text,chatId:integer,fromUserId:integer
+
+### Migrate
+
+sequelize db:migrate
+
+### Chat seeder file
+
+sequelize seed:create --name chats
+
+sequelize db:seed --seed [filename]
+
+sequelize db:seed --seed 20210313182502-chats
---
+++ models/message.js
@@ -0,0 +1,43 @@
+'use strict';
+const { Model } = require('sequelize');
+
+const config = require('../config/app');
+
+module.exports = (sequelize, DataTypes) => {
+	class Message extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			this.belongsTo(models.Chat, { foreignKey: 'chatId' });
+			this.belongsTo(models.Chat, { foreignKey: 'fromUserId' });
+		}
+	}
+	Message.init(
+		{
+			type: DataTypes.STRING,
+			message: {
+				type: DataTypes.TEXT,
+				get() {
+					const type = this.getDataValue('type');
+					const id = this.getDataValue('chatId');
+					const content = this.getDataValue('message');
+
+					return type === 'text'
+						? content
+						: `${config.appUrl}:${config.appPort}/chat/${id}/${content}`;
+				},
+			},
+			chatId: DataTypes.INTEGER,
+			fromUserId: DataTypes.INTEGER,
+		},
+		{
+			sequelize,
+			modelName: 'Message',
+		}
+	);
+	return Message;
+};
